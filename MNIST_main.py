4e90843e00d5f43ca9ec13c55493aacc1c3f56e6
MOHD AFFAN <affanmalikstg@gmail.com>
2026-06-13 14:56:43 +0530

    Updated Test

--- MNIST_main.py
+++ MNIST_main.py
@@ -9,6 +9,6 @@ W1, B1, W2, B2, W3, B3 = load_model()
 
 Pred = predict(W1, B1, W2, B2, W3, B3 , x_test)
 
-print(Pred[:10])
-print(y_test[:10])
+print("Prediction of My Model on test set (First 10 images): ",Pred[:10])
+print("True Labels of images on test set : ",y_test[:10])
 
