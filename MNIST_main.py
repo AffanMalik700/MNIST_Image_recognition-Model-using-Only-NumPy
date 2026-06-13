4e802a27663bae86013a840628a005f6781994bf
MOHD AFFAN <affanmalikstg@gmail.com>
2026-06-14 01:35:26 +0530

    Added show function using matplotlib to display the prediction on image using given input index

--- MNIST_main.py
+++ MNIST_main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 from src.data_loader import load_data
 from src.train import predict , load_model ,accuracy
 
@@ -9,7 +9,17 @@ W1, B1, W2, B2, W3, B3 = load_model()
 
 Pred = predict(W1, B1, W2, B2, W3, B3 , x_test)
 
-print("Prediction of My Model on test set (First 10 images): ",Pred[:10])
-print("True Labels of images on test set : ",y_test[:10])
-print("Accuracy on test set : ", np.mean(Pred == y_test) * 100)
+def show_image(image, predicted, true_label):
+    plt.imshow(image.reshape(28, 28), cmap='gray')
+    plt.title(f"Predicted: {predicted} | True: {true_label}")
+    plt.axis('off')
+    plt.show()
+
+i = 856
+
+show_image(x_test[i], Pred[i], y_test[i])
+
+# print("Prediction of My Model on test set (First 10 images): ",Pred[:10])
+# print("True Labels of images on test set : ",y_test[:10])
+# print("Accuracy on test set : ", np.mean(Pred == y_test) * 100)
 
